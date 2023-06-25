e988bd004722d7a41e8ec8877cf181f8c764b4dd
Patrik Šimanský <Gmod4phun@gmail.com>
2023-06-25 19:17:57 +0200

    Use newer USP model for Pistol

--- code/weapons/Pistol.cs
+++ code/weapons/Pistol.cs
@@ -1,21 +1,52 @@
-﻿using Sandbox;
+﻿﻿using Sandbox;
 
 [Spawnable]
 [Library( "weapon_pistol", Title = "Pistol" )]
 partial class Pistol : Weapon
 {
-	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
-
+	public override float ReloadTime => 1.5f;
 	public override float PrimaryRate => 15.0f;
 	public override float SecondaryRate => 1.0f;
 
 	public TimeSince TimeSinceDischarge { get; set; }
 
+	public AnimatedEntity ViewModelArms { get; set; }
+
 	public override void Spawn()
 	{
 		base.Spawn();
 
-		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+		Model = Cloud.Model( "https://asset.party/facepunch/w_usp" );
+		SetBodyGroup( "barrel", 1 );
+		//SetBodyGroup( "sights", 1 );
+		LocalScale = 2f; // todo - this doesn't work when bone merged! we should make it multiply!
+	}
+	public override void CreateViewModel()
+	{
+		ViewModelEntity = new ViewModel();
+		ViewModelEntity.Position = Position;
+		ViewModelEntity.Owner = Owner;
+		ViewModelEntity.EnableViewmodelRendering = true;
+		ViewModelEntity.Model = Cloud.Model( "https://asset.party/facepunch/v_usp" );
+		ViewModelEntity.SetBodyGroup( "barrel", 1 );
+		//ViewModelEntity.SetBodyGroup( "sights", 0 );
+
+		ViewModelArms = new AnimatedEntity( "models/first_person/first_person_arms.vmdl" );
+		ViewModelArms.SetParent( ViewModelEntity, true );
+		ViewModelArms.EnableViewmodelRendering = true;
+	}
+	public override void ActiveStart( Entity ent )
+	{
+		base.ActiveStart( ent );
+
+		ViewModelEntity?.SetAnimParameter( "b_deploy", true );
+	}
+
+	public override void Reload()
+	{
+		base.Reload();
+
+		ViewModelEntity?.SetAnimParameter( "b_reload", true );
 	}
 
 	public override bool CanPrimaryAttack()
@@ -29,10 +60,11 @@ partial class Pistol : Weapon
 		TimeSinceSecondaryAttack = 0;
 
 		(Owner as AnimatedEntity)?.SetAnimParameter( "b_attack", true );
+		ViewModelEntity?.SetAnimParameter( "b_attack", true );
 
 		ShootEffects();
 		PlaySound( "rust_pistol.shoot" );
-		ShootBullet( 0.05f, 1.5f, 9.0f, 3.0f );
+		ShootBullet( 0.01f, 1.5f, 9.0f, 3.0f );
 	}
 
 	private void Discharge()
@@ -52,7 +84,7 @@ partial class Pistol : Weapon
 
 		ApplyAbsoluteImpulse( rot.Backward * 200.0f );
 	}
-
+	
 	protected override void OnPhysicsCollision( CollisionEventData eventData )
 	{
 		if ( eventData.Speed > 500.0f )
