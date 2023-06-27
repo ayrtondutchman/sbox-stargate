e778fd6224882e3b81d0c57875de25f58598c69d
Patrik Šimanský <Gmod4phun@gmail.com>
2023-06-27 10:39:40 +0200

    Added lights to Universe Ramp

--- code/sbox_stargate/entities/ramps/UniverseRamp.cs
+++ code/sbox_stargate/entities/ramps/UniverseRamp.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Sandbox;
 
 [Title( "Universe Ramp" ), Category( "Stargate" ), Icon( "chair" ), Spawnable]
 public partial class UniverseRamp : Prop, IStargateRamp, IGateSpawner
 {
-
 	[Net]
 	public Vector3 SpawnOffset { get; private set; } = new( 0, 0, 0 );
 	public int AmountOfGates => 1;
+
+	public List<CapsuleLightEntity> Lights = new();
 
 	public Vector3[] StargatePositionOffset => new Vector3[] {
 		new Vector3( -108.75f, 0, 135 )
@@ -29,6 +31,63 @@ public partial class UniverseRamp : Prop, IStargateRamp, IGateSpawner
 		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, true );
 
 		Tags.Add( "solid" );
+
+		PostSpawn();
+	}
+
+	public async void PostSpawn()
+	{
+		await GameTask.NextPhysicsFrame();
+
+		for (var i = 1; i <= 4; i++)
+		{
+			var t = (Transform)GetAttachment( $"light{i}" );
+			var light = new CapsuleLightEntity();
+			light.Color = Color.FromBytes( 230, 215, 240 );
+			light.CapsuleLength = 48;
+			light.LightSize = 0.2f;
+			light.Position = t.Position;
+			light.Rotation = t.Rotation;
+			light.Brightness = 0;
+			light.SetParent( this );
+			Lights.Add( light );
+		}
+
+		for ( var i = 5; i <= 8; i++ )
+		{
+			var t = (Transform)GetAttachment( $"light{i}" );
+			var light = new CapsuleLightEntity();
+			light.Color = Color.FromBytes( 230, 215, 240 );
+			light.CapsuleLength = 22;
+			light.LightSize = 0.2f;
+			light.Position = t.Position;
+			light.Rotation = t.Rotation;
+			light.Brightness = 0;
+			light.SetParent( this );
+			Lights.Add( light );
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		foreach (var light in Lights)
+		{
+			light?.Delete();
+		}
+	}
+
+	[GameEvent.Tick.Server]
+	private void LightThink()
+	{
+		var gate = Gate.FirstOrDefault();
+		var shouldGlow = gate.IsValid() ? !gate.Idle : false;
+
+		foreach ( var light in Lights )
+		{
+			light.Brightness = light.Brightness.LerpTo( shouldGlow ? 0.1f : 0f, Time.Delta * (shouldGlow ? 4 : 16) );
+		}
 	}
 
 	public void FromJson( JsonElement data )
