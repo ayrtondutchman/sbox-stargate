496371e5dd791b242cdb04aae2c92b8fc9fb7d36
Patrik Šimanský <Gmod4phun@gmail.com>
2023-06-30 21:51:34 +0200

    Test for applying collission rules at runtime

--- code/Game.cs
+++ code/Game.cs
@@ -1,6 +1,9 @@
 ﻿using Sandbox;
+using Sandbox.Physics;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static Sandbox.Physics.CollisionRules;
 
 partial class SandboxGame : GameManager
 {
@@ -14,6 +17,47 @@ partial class SandboxGame : GameManager
 			//Log.Info( $"The world is {Game.WorldEntity}" );
 
 			//Game.WorldEntity.Tags.Remove( "solid" );
+		}
+	}
+
+	// this isnt used for now, just something that could probably be used if gates would become an addon
+	public static void ApplyStargateCollisionRules()
+	{
+		if ( Game.IsServer )
+		{
+			CollisionRules rules = new CollisionRules();
+
+			rules.Defaults = new Dictionary<string, Result>();
+			rules.Defaults["solid"] = Result.Collide;
+			rules.Defaults["trigger"] = Result.Trigger;
+			rules.Defaults["ladder"] = Result.Ignore;
+			rules.Defaults["water"] = Result.Trigger;
+			rules.Defaults[StargateTags.FakeWorld] = Result.Ignore;
+
+			rules.Pairs = new HashSet<Pair>();
+			rules.Pairs.Add( new Pair( "solid", "solid", Result.Collide ) );
+			rules.Pairs.Add( new Pair( "solid", "trigger", Result.Trigger ) );
+			rules.Pairs.Add( new Pair( "trigger", "glass", Result.Ignore ) );
+			rules.Pairs.Add( new Pair( "debris", "player", Result.Ignore ) );
+			rules.Pairs.Add( new Pair( "player", "glass", Result.Trigger ) );
+			rules.Pairs.Add( new Pair( "glass", "glass", Result.Ignore ) );
+
+			rules.Pairs.Add( new Pair( StargateTags.BehindGate, StargateTags.InBufferFront, Result.Ignore ) );
+			rules.Pairs.Add( new Pair( StargateTags.BehindGate, StargateTags.BeforeGate, Result.Ignore ) );
+
+			rules.Pairs.Add( new Pair( StargateTags.BeforeGate, StargateTags.InBufferBack, Result.Ignore ) );
+
+			rules.Pairs.Add( new Pair( StargateTags.InBufferFront, StargateTags.FakeWorld, Result.Collide ) );
+			rules.Pairs.Add( new Pair( StargateTags.InBufferFront, "world", Result.Ignore ) );
+
+			rules.Pairs.Add( new Pair( StargateTags.InBufferFront, StargateTags.InBufferBack, Result.Ignore ) );
+
+			rules.Pairs.Add( new Pair( StargateTags.InBufferBack, StargateTags.FakeWorld, Result.Collide ) );
+			rules.Pairs.Add( new Pair( StargateTags.InBufferBack, "world", Result.Ignore ) );
+
+			Game.PhysicsWorld.SetCollisionRules( rules );
+
+			Log.Info( "Applied SG collision rules" );
 		}
 	}
 
