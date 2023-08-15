f3ebf0bacd6ef1c58a075b67e81ba2bb70f73a28
ayrtondutchman <47227481+ayrtondutchman@users.noreply.github.com>
2023-08-16 01:02:28 +0200

    Forgot to git add RingRing.cs

--- code/sbox_stargate/entities/rings_base/RingRing.cs
+++ code/sbox_stargate/entities/rings_base/RingRing.cs
@@ -28,7 +28,7 @@ public partial class RingRing : KeyframeEntity {
 
 		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, true );
 		PhysicsBody.BodyType = PhysicsBodyType.Static;
-		EnableAllCollisions = true;
+		EnableAllCollisions = false;
 		RenderColor = RenderColor.WithAlpha( 0 );
 	}
 
@@ -50,6 +50,7 @@ public partial class RingRing : KeyframeEntity {
 
 	public void MoveUp() {
 		RenderColor = RenderColor.WithAlpha(1);
+		EnableAllCollisions = true;
 		ShouldRetract = false;
 		Move();
 	}
